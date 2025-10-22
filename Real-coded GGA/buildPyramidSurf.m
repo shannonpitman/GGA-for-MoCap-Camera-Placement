a5ec39337d0d5374ced75084e4f5fffc337e65e8
shannonpitman <ptmsha001@myuct.ac.za>
2025-10-22 21:22:15 +0200

    Vectorised buildPyramidSurf and removed the need for a helper function (i.e calcPyramidSurf)

--- Real-coded GGA/buildPyramidSurf.m
+++ Real-coded GGA/buildPyramidSurf.m
@@ -1,13 +1,23 @@
-function surfStruct = buildPyramidSurf(cameraCentre, worldPoint, adj)
+function planes = buildPyramidSurf(cameraCentre, worldPoint, adj)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-surfStruct = struct('n', {}, 'd', {});
+corner1 = worldPoint(:,adj(:,1));
+corner2 = worldPoint(:,adj(:,2));
 
-for a = 1:4
-    c1 = worldPoint(:,adj(a,1));
-    c2 = worldPoint(:,adj(a,2));
-    [n, d] = calcPyramidSurf(cameraCentre, c1, c2);
-    surfStruct(a).n = n; %normal
-    surfStruct(a).d = d; %constant 
-end
-end
+vector1 = corner1 - cameraCentre; 
+vector2 = corner2 - cameraCentre;
+planes.n = cross(vector2, vector1, 1);  %normal to the plane of surface 
+planes.n = surfNormal./vecnorm(surfNormal,2,1); %normalise to only extract direction
+planes.d = sum(planes.n .* cameraCentre,1); %constant of plane calculated by dot product of vector2 (corner 2) with a point on the surface (perspective centre)
+
+
+
+%planes = struct('n', [], 'd', []);
+% for a = 1:4
+%     c1 = worldPoint(:,adj(a,1));
+%     c2 = worldPoint(:,adj(a,2));
+%     [n, d] = calcPyramidSurf(cameraCentre, c1, c2);
+%     planes(a).n = n; %normal
+%     planes(a).d = d; %constant 
+% end
+% end
--- Real-coded GGA/calcPyramidSurf.m
+++
@@ -1,9 +0,0 @@
-function [surfNormal,surfConst] = calcPyramidSurf(cameraCentre,corner1, corner2)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-    vector1 = corner1-cameraCentre; 
-    vector2 = corner2-cameraCentre;
-    surfNormal= cross((vector2), (vector1));  %normal to the plane of surface 
-    surfNormal = surfNormal/norm(surfNormal); %normalise to only extract direction
-    surfConst= dot(surfNormal, cameraCentre); %constant of plane calculated by dot product of vector2 (corner 2) with a point on the surface (perspective centre)
-end
