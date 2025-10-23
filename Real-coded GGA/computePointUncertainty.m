88f391bdd69bd6b4e6ea9e92f3c6b09c5756c229
shannonpitman <ptmsha001@myuct.ac.za>
2025-10-23 11:48:57 +0200

    Two additional helper functions. Prevents crossover culminating in cameras being in between boundary values ( keeps them along the boundary and rather just swaps certain aspects of the chromosome). Moved a part of resUncertainty to be a helper function so that the parallel cores of parfor can work.

---
+++ Real-coded GGA/computePointUncertainty.m
@@ -0,0 +1,52 @@
+function uncertainty = computePointUncertainty(point, cameras, numCams, resolution, adjacentSurfaces, du,dv, penaltyUncertainty, w2)
+    
+    isVisible = false(numCams,1); %logical vector to check whether a camera can see point
+    cameraCentres = zeros(numCams,1);
+    worldPoints = zeros(numCams, 1);
+    planes = zeros(numCams, 1);
+    
+    for i = 1:numCams
+        uv = cameras{i}.project(point); %uv projected coordinates 
+        u = uv(1);
+        v = uv(2);
+        if (u >= 1 && u <= resolution(1) && v >= 1 && v <= resolution(2))
+            cameraCentres{i} = cameras{i}.center().'; %world location of Camera center
+            worldPoints{i} = quantToWorld(cameras{i}, uv{i}, du, dv, cameraCentres{i});
+            planes{i} = buildPyramidSurf(cameraCentres{i}, worldPoints{i}, adjacentSurfaces);
+            isVisible(i) = true;
+        end
+    end
+
+    visibleIdx = find(isVisible);
+    numVisible = length(visibleIdx);
+
+
+    if numVisible ==0 %if there are less than 2 cameras then triangulation is not possible 
+        uncertainty = penaltyUncertainty; %point is not visible in any cameras FOV
+    elseif numVisible==1 %if at least one camera sees the point
+        uncertainty = 0.5*penaltyUncertainty;
+    else
+        vertices = calcVertices(numVisible,visibleIdx(:).', adjacentSurfaces, planes);
+        unique_vertices = unique(round(vertices,6),'rows', 'stable');
+        % size(unique_vertices);
+        V_ = unique_vertices-point; %centre the points at the mean 
+        C0v_vert = cov(V_); %covariance matrix
+        [A,~] = eig(C0v_vert); %[eigenvectors, eigenvalues in a diagonal matrix]
+        transformed_v = A*V_.';%rotated points 
+
+        %Initial guess
+        axes0 = max(abs(transformed_v.'), [], 1).' ; %1x3 [a0,b0,c0], max magnitude of the x,y,z components of v 
+
+        fun = @(axes) optimiseEllipsoid(axes, transformed_v, axes0, w2);
+        x0 = axes0;
+        options = optimset('Display', 'notify', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxIter', 1000, 'MaxFunEvals', 5000);
+        [axes_opt, ~] = fminsearch(fun, x0, options);
+
+        %optimised axes [mm]
+        a = axes_opt(1)*1000;
+        b = axes_opt(2)*1000;
+        c = axes_opt(3)*1000;
+
+        uncertainty = 4/3*pi*a*b*c;%mm^3
+    end
+end
