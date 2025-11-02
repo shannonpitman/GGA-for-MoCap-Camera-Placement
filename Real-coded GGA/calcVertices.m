688d9e1f089417a56a68555cc8008e0b3520661f
shannonpitman <ptmsha001@myuct.ac.za>
2025-11-02 20:46:56 +0200

    Vectorised visibility check

--- Real-coded GGA/calcVertices.m
+++ Real-coded GGA/calcVertices.m
@@ -1,52 +1,57 @@
 function vertices = calcVertices(numVisible, visibleIdx, adj, planes)
 % Summary of this function goes here
 %   Detailed explanation goes here
-    vertices = zeros(16*numVisible, 3); %16 = 4 pairs of adj surface of one cam intersected with 4 surfaces on other pyrsamid 
+    maxVertices = 16*nchoosek(numVisible,2); %16 = 4 pairs of adj surface of one cam intersected with 4 surfaces on other pyrsamid 
+    vertices = zeros(maxVertices,3);
     idx = 1;
     tol = -1e-9;
 
-
+    
     %Plane params for cameras that can see the point 
     visiblePlanes = cell(numVisible,1);
     for i = 1:numVisible
-        visiblePlanes{i} = planes{visibleIdx(i)}; %index plane array at visible camera 
+        visiblePlanes{i} = planes{visibleIdx(i)};
     end
-    for i = 1:numVisible
-        cam_surfaces = visiblePlanes{i};
+
+    camPairs = nchoosek(1:numVisible,2);
+    numPairs = size(camPairs,1);
+
+    % Camera Pairs
+    for pairIdx = 1:numPairs
+        i = camPairs(pairIdx, 1);
+        k = camPairs(pairIdx, 2);
+
+        %index plane array at visible camera 
+        cam_surfacesI = planes{visibleIdx(i)}; 
+        cam_surfacesK = planes{visibleIdx(k)}; 
+        
         for j =1:4
-            n1 = cam_surfaces(1:3,adj(j,1));
-            d1 = cam_surfaces(4, adj(j,1));
-            n2 = cam_surfaces(1:3,adj(j,2));
-            d2 = cam_surfaces(4,adj(j,2));
-            
+            n1 = cam_surfacesI(1:3,adj(j,1));
+            d1 = cam_surfacesI(4, adj(j,1));
+            n2 = cam_surfacesI(1:3,adj(j,2));
+            d2 = cam_surfacesI(4,adj(j,2));
             %Process at all other pyramids 
-            for k = 1:numVisible
-                if k == i
-                    continue; 
-                end 
+            n3All = cam_surfacesK(1:3,:);
+            d3All = cam_surfacesK(4,:);
+            
+            for l = 1:size(n3All,2)
+                n3 = n3All(:,l);
+                d3 = d3All(:,l);
                 
-                other_surfaces = visiblePlanes{k};
-                
-                %Loop over each surface from other pyramid
-                for l = 1:size(other_surfaces,2)
-                    n3 = other_surfaces(1:3,l);
-                    d3 = other_surfaces(4,l);
-                    
-                    A = [n1.'; n2.'; n3.'];
+                A = [n1.'; n2.'; n3.'];
                    
-                    if abs(det(A)) < 1e-9 %check that planes aren't parraell and thus illconditioned -> no intesection 
-                         continue;
-                    end
-                    %least squares solve -> finds error region around point its trying to locate
-                    b = [d1; d2; d3];
-                    x = A\b; 
-                    
-                     if isInsidePlanes(x, visiblePlanes, tol,numVisible)
-                        vertices(idx, :) = x.'; %row x,y,z
-                        idx = idx +1;
-                     end
+                if abs(det(A)) < -tol %check that planes aren't parraell and thus illconditioned -> no intesection 
+                     continue;
                 end
-            end
+                %least squares solve -> finds error region around point its trying to locate
+                b = [d1; d2; d3];
+                x = A\b; 
+                    
+                 if isInsidePlanes(x, visiblePlanes, tol, numVisible)
+                    vertices(idx, :) = x.'; %row x,y,z
+                    idx = idx +1;
+                 end
+             end
         end
     end
     vertices = vertices(1:idx-1,:);
--- Real-coded GGA/computePointUncertainty.m
+++ Real-coded GGA/computePointUncertainty.m
@@ -1,52 +1,55 @@
 function uncertainty = computePointUncertainty(point, cameras, numCams, resolution, adjacentSurfaces, du,dv, penaltyUncertainty, w2)
-    isVisible = false(numCams,1); %logical vector to check whether a camera can see point
+    uv = zeros(numCams,2);
     planes = cell(1, numCams);
-
+    
     for i = 1:numCams
-        uv = cameras{i}.project(point); %uv projected coordinates 
-        u = uv(1);
-        v = uv(2);
-
-        if (u >= 1 && u <= resolution(1) && v >= 1 && v <= resolution(2))
-            cameraCentres = cameras{i}.center().'; %world location of Camera center
-            worldPoints = quantToWorld(cameras{i}, u,v, du, dv, cameraCentres);
-            planes{i} = buildPyramidSurf(cameraCentres, worldPoints, adjacentSurfaces);
-            isVisible(i) = true;
-        end
+        uv(i,:) = cameras{i}.project(point); %uv projected coordinates 
     end
 
+    isVisible = (uv(:,1) >= 1) & (uv(:,1) <= resolution(1)) & (uv(:,2) >= 1) & (uv(:,2) <= resolution(2));
     visibleIdx = find(isVisible);
     numVisible = length(visibleIdx);
-
-
-    if numVisible == 0 %if there are less than 2 cameras then triangulation is not possible 
+    
+    %early exit if trinagulation is not possible
+    if numVisible == 0 
         uncertainty = penaltyUncertainty; %point is not visible in any cameras FOV
+        return;
     elseif numVisible== 1 %if at least one camera sees the point
         uncertainty = 0.5*penaltyUncertainty;
-    else
-        vertices = calcVertices(numVisible, visibleIdx, adjacentSurfaces, planes);
-        unique_vertices = unique(round(vertices,6),'rows', 'stable');
-
-        V_ = unique_vertices-point; %centre the points at the mean 
-        C0v_vert = cov(V_); %covariance matrix
-        [A,~] = eig(C0v_vert); %[eigenvectors, eigenvalues in a diagonal matrix]
-        
-        
-        transformed_v = (A*V_.').';%rotated points 
-
-        %Initial guess
-        axes0 = max(abs(transformed_v), [], 1).' ; %1x3 [a0,b0,c0], max magnitude of the x,y,z components of v 
-
-        fun = @(axes) optimiseEllipsoid(axes, transformed_v.', axes0, w2);
-        options = optimset('Display', 'off', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxIter', 1000, 'MaxFunEvals', 5000);
-        [axes_opt, ~] = fminsearch(fun, axes0, options);
-
-        %optimised axes [m]
-        a = abs(axes_opt(1));
-        b = abs(axes_opt(2));
-        c = abs(axes_opt(3));
-
-        uncertainty = 4/3*pi*a*b*c;%m^3
-        
+        return;
     end
+
+   for i = visibleIdx'
+       u = uv(i,1);
+       v = uv(i,2);
+       cameraCentres = cameras{i}.center().'; %world location of Camera center
+       worldPoints = quantToWorld(cameras{i}, u,v, du, dv, cameraCentres);
+       planes{i} = buildPyramidSurf(cameraCentres, worldPoints, adjacentSurfaces);
+   end
+
+    vertices = calcVertices(numVisible, visibleIdx, adjacentSurfaces, planes);
+    unique_vertices = unique(round(vertices,6),'rows', 'stable');
+
+    V_ = unique_vertices-point; %centre the points at the mean 
+    C0v_vert = cov(V_); %covariance matrix
+    [A,~] = eig(C0v_vert); %[eigenvectors, eigenvalues in a diagonal matrix]
+    
+    
+    transformed_v = (A*V_.').';%rotated points 
+
+    %Initial guess
+    axes0 = max(abs(transformed_v), [], 1).' ; %1x3 [a0,b0,c0], max magnitude of the x,y,z components of v 
+
+    fun = @(axes) optimiseEllipsoid(axes, transformed_v.', axes0, w2);
+    options = optimset('Display', 'off', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxIter', 1000, 'MaxFunEvals', 5000);
+    [axes_opt, ~] = fminsearch(fun, axes0, options);
+
+    %optimised axes [m]
+    a = abs(axes_opt(1));
+    b = abs(axes_opt(2));
+    c = abs(axes_opt(3));
+
+    uncertainty = 4/3*pi*a*b*c;%m^3
+        
 end
+
